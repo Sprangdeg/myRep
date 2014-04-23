523443564827ce157c15249e14fc7f24fdf547ec
Daniel Bonstrom <d.bonstrom@gmail.com>
2014-04-23 18:34:55 +0100

    some functionallity

--- www/js/timedCharge.js
+++ www/js/timedCharge.js
@@ -1,6 +1,10 @@
 $("#getPriceButton").click(function(){
-			getPrice();
-			//alert($("#targetTime").val());		
+			getPrice();		
+		});
+
+$("#confirmChargeButton").click(function(){
+			setTimedCharge();
+	
 		});
 
 function getPrice()
@@ -21,13 +25,52 @@ function getPrice()
     });
 }
 
+function setTimedCharge()
+{
+    if($("#estimatedPrice").val()=="" || $("#estimatedPrice").val()=="Not Possible"){
+  		$("#infoLabel").load("error.html", function() {   
+			$("#error").text("Error: Not possible to set charge");
+		}
+	);
+    }  
+    else{
+    $.ajax({
+        	type: 'POST',
+        	data: {	'request': 'setTimedCharge', 
+        			'targetCharge': $("#targetChargeSlider").val(),
+        			'targetTime': $("#targetTime").val() },
+			url: 'http://danu6.it.nuigalway.ie/bonstrom/project/server.php',
+			timeout: 3000,
+				 success: function(response){
+					printResponse(response);			
+			 },
+			 	error: function(){
+					printError("Failed to connect to server");
+				 }  
+    	});
+    }
+}
+
+function printResponse(response){
+	//The function is a "callback". Since the code would not find the tags in the newly loaded chargingResponse.html in time
+//it has to be called again when it has been loaded.
+		$("#infoLabel").load("chargingResponse.html", function() {   
+ 		$("#type").text(response.chargingType);
+ 		$("#rate").text(response.chargingRate);
+ 		$("#targetCharge").text(response.targetCharge);
+ 		$("#timeLeft").text(convertTime(response.timeLeft)); 
+		}
+	);
+}
+
+
 function printEstimatedPrice(response){
 var price = jQuery.parseJSON(response);
 
 	if(price==0)
 		$("#estimatedPrice").text("Not Possible");
 	else
-		$("#estimatedPrice").text("€ " + price);
+		$("#estimatedPrice").text(price);
 }
 
 function loadCharge(){
